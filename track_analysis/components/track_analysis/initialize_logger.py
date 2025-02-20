ffbddc44b2289fcf8edc47b68ee8e1f2924a4b85
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-02-20 18:47:27 +0100

    Set up logging

---
+++ track_analysis/components/track_analysis/initialize_logger.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from track_analysis.components.md_common_python.py_common.logging import HoornLogger, LogType, FileHoornLogOutput, \
+    DefaultHoornLogOutput
+
+
+def _get_user_local_appdata_dir() -> Path:
+    # Get OS Name
+    import platform
+    system = platform.system()
+
+    if system == "Windows":
+        return Path(Path.home()).joinpath("AppData", "Local")
+    elif system == "Darwin":
+        return Path(Path.home()).joinpath("Library", "Application Support")
+    elif system == "Linux":
+        return Path(Path.home()).joinpath(".local", "share")
+
+    raise ValueError("Unsupported operating system")
+
+def get_logger() -> HoornLogger:
+    max_sep_length = 30
+
+    file_output: FileHoornLogOutput = FileHoornLogOutput(
+        log_directory=_get_user_local_appdata_dir().joinpath("Track Analysis", "logs"),
+        max_logs_to_keep=10,
+        max_separator_length=max_sep_length
+    )
+
+    return HoornLogger(
+        outputs=[file_output, DefaultHoornLogOutput(max_separator_length=max_sep_length)],
+        min_level=LogType.DEBUG,
+        separator_root="TrackAnalysis",
+        max_separator_length=max_sep_length)
--- track_analysis/components/track_analysis/main.py
+++ track_analysis/components/track_analysis/main.py
@@ -1,11 +1,9 @@
 from track_analysis.components.md_common_python.py_common.cli_framework import CommandLineInterface
-from track_analysis.components.md_common_python.py_common.logging import HoornLogger, LogType
+from track_analysis.components.md_common_python.py_common.logging import HoornLogger
+from track_analysis.components.track_analysis.initialize_logger import get_logger
 
 if __name__ == "__main__":
-    logger: HoornLogger = HoornLogger(
-        min_level=LogType.DEBUG,
-        separator_root="TrackAnalysis",
-        max_separator_length=30)
+    logger: HoornLogger = get_logger()
 
     cmd: CommandLineInterface = CommandLineInterface(logger)
     cmd.start_listen_loop()
